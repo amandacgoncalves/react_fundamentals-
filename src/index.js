efaaa6a93329a8bcc69fb54278627500a5115715
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-05-24 11:01:22 -0300

    component w prop

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 import First from './components/basic/First'
+import WParams from './components/basic/WParams'
 
 ReactDOM.render(
     <div>
     <First></First>
+    <WParams 
+    title="Situação do Aluno" 
+    aluno="Pedro" 
+    nota={9.3} 
+    />
     </div>, 
     document.getElementById('root')
 );
