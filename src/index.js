84ff6fccf8d572f0d6839333f032d38a238a8c95
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-05-24 10:13:46 -0300

    loading css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import './index.css'
 import ReactDOM from 'react-dom'
+import React from 'react'
+
+const tag = <strong>Hello World!</strong>
 
 ReactDOM.render(
-    'Hello World', 
+    <div>
+        {tag}
+    </div>, 
     document.getElementById('root')
 );
