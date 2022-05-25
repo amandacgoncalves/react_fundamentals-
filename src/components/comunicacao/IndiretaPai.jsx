18b2d4f72d698bb15917b99996a795e1646198e6
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-05-25 12:40:25 -0300

    component state

--- src/components/comunicacao/IndiretaPai.jsx
+++ src/components/comunicacao/IndiretaPai.jsx
@@ -1,28 +1,31 @@
-import React from "react";
-import IndiretaFilho from './IndiretaFilho'
+import React, { useState } from "react";
+import IndiretaFilho from "./IndiretaFilho";
 
-export default props => {
-    let nome = '?'
-    let idade = 0
-    let nerd = false
+export default (props) => {
+    const [a, b] = [1, 2]
+  const [nome, setNome] = useState("?");
+  const [idade, setIdade] = useState(0);
+  const [nerd, setNerd] = useState(false);
+  
 
+  function fornecerInformacoes(nome, idade, nerd) {
+   
+    setNome(nome)
+    setIdade(idade)
+    setNerd(nerd)
 
-    function fornecerInformacoes(nomeParam, idadeParam, nerdParam) {
-        nome = nomeParam
-        idade = idadeParam
-        nerd = nerdParam
-        console.log(nome, idade, nerd)
-    }
+  }
 
-
-    return (
-        <div>
-            <div>
-                <span>{nome} </span>
-                <span><strong>{idade}</strong></span>
-                <span> {nerd ? 'Verdadeiro' : 'Falso'}</span>
-            </div>
-            <IndiretaFilho quandoClicar={fornecerInformacoes}></IndiretaFilho>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>
+        <span>{nome} </span>
+        <span>
+          <strong>{idade}</strong>
+        </span>
+        <span> {nerd ? "Verdadeiro" : "Falso"}</span>
+      </div>
+      <IndiretaFilho quandoClicar={fornecerInformacoes}></IndiretaFilho>
+    </div>
+  );
+};
